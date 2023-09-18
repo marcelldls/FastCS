ca3c77e71ef5e7ad007acc5cb036fc9f9592fcf7
Marcell Nagy <marcell.nagy@diamond.ac.uk>
2023-09-18 15:45:42 +0100

    Type hinting

--- src/fastcs/backends/tango/dsr.py
+++ src/fastcs/backends/tango/dsr.py
@@ -1,14 +1,16 @@
 from dataclasses import dataclass
 from types import MethodType
-from typing import Any, Callable
+from typing import Any, List, Awaitable, Callable
 
-from tango import AttrWriteType, Database, DbDevInfo, DevState, GreenMode, server
+import tango
+from tango import server, AttrWriteType, Database, DbDevInfo, DevState
 
 from fastcs.attributes import AttrR, AttrRW, AttrW
 from fastcs.backend import (
     _link_attribute_sender_class,
     _link_single_controller_put_tasks,
 )
+from fastcs.controller import BaseController
 from fastcs.datatypes import Bool, DataType, Float, Int
 from fastcs.exceptions import FastCSException
 from fastcs.mapping import Mapping
@@ -22,19 +24,22 @@ class TangoDSROptions:
     debug: bool = False
 
 
-def _get_dtype_args(datatype: DataType) -> dict:
+def _get_dtype_args(datatype: DataType) -> dict[str, Any]:
     match datatype:
-        case Bool(znam, onam):
+        case Bool():
             return {"dtype": bool}
         case Int():
             return {"dtype": int}
         case Float(prec):
             return {"dtype": float, "format": f"%.{prec}"}
         case _:
-            raise FastCSException(f"Unsupported type {type(datatype)}: {datatype}")
+            msg = f"Unsupported type {type(datatype)}: {datatype}"
+            raise FastCSException(msg)
 
 
-def _pick_updater_fget(attr_name, attribute, controller):
+def _pick_updater_fget(
+    attr_name: str, attribute: AttrR, controller: BaseController
+) -> Awaitable[Any]:
     async def fget(self):
         await attribute.updater.update(controller, attribute)
         self.info_stream(f"called fget method: {attr_name}")
@@ -43,7 +48,9 @@ def _pick_updater_fget(attr_name, attribute, controller):
     return fget
 
 
-def _pick_updater_fset(attr_name, attribute, controller):
+def _pick_updater_fset(
+    attr_name: str, attribute: AttrW, controller: BaseController
+) -> Awaitable[None]:
     async def fset(self, val):
         await attribute.updater.put(controller, attribute, val)
         self.info_stream(f"called fset method: {attr_name}")
@@ -51,45 +58,55 @@ def _pick_updater_fset(attr_name, attribute, controller):
     return fset
 
 
-def _collect_dev_attributes(mapping: Mapping) -> dict:
+def _collect_dev_attributes(mapping: Mapping) -> dict[str, Any]:
     collection = {}
-    for s_map in mapping.get_controller_mappings():
-        path = s_map.controller.path
+    for single_mapping in mapping.get_controller_mappings():
+        path = single_mapping.controller.path
 
-        for attr_name, attribute in s_map.attributes.items():
+        for attr_name, attribute in single_mapping.attributes.items():
             attr_name = attr_name.title().replace("_", "")
-            dev_attr_name = path.upper() + "_" + attr_name if path else attr_name
+            d_attr_name = f"{path.upper()}_{attr_name}" if path else attr_name
 
-            instance = {"label": dev_attr_name}
+            instance = {"label": d_attr_name}
             instance.update(_get_dtype_args(attribute.datatype))
-
-            polling_period = int(attribute.updater.update_period * 1e3)  # ms
-            f_args = (attr_name, attribute, s_map.controller)
 
             match attribute:
                 case AttrRW():
-                    instance["fget"] = _pick_updater_fget(*f_args)
-                    instance["fset"] = _pick_updater_fset(*f_args)
+                    instance["fget"] = _pick_updater_fget(
+                        attr_name, attribute, single_mapping.controller
+                    )
+                    instance["fset"] = _pick_updater_fset(
+                        attr_name, attribute, single_mapping.controller
+                    )
                     instance["access"] = AttrWriteType.READ_WRITE
-                    instance["polling_period"] = polling_period
+                    if attribute.updater is not None:
+                        polling_period = int(attribute.updater.update_period)
+                        instance["polling_period"] = polling_period * 1000
                 case AttrR():
                     # instance["fget"] = lambda *args: 1  # Read one/True
-                    instance["fget"] = _pick_updater_fget(*f_args)
+                    instance["fget"] = _pick_updater_fget(
+                        attr_name, attribute, single_mapping.controller
+                    )
                     instance["access"] = AttrWriteType.READ
-                    instance["polling_period"] = polling_period
+                    if attribute.updater is not None:
+                        polling_period = int(attribute.updater.update_period)
+                        instance["polling_period"] = polling_period * 1000
                 case AttrW():
                     # instance["fset"] = lambda *args: None  # Do nothing
-                    instance["fset"] = _pick_updater_fset(*f_args)
+                    instance["fset"] = _pick_updater_fset(
+                        attr_name, attribute, single_mapping.controller
+                    )
                     instance["access"] = AttrWriteType.WRITE
-                    instance["polling_period"] = polling_period
 
-            collection[dev_attr_name] = server.attribute(**instance)
+            collection[d_attr_name] = server.attribute(**instance)
 
     return collection
 
 
-def _pick_command_f(method_name, method, controller):
-    async def _dynamic_f(self):
+def _pick_command_f(
+    method_name: str, method: Callable, controller: BaseController
+) -> Callable[..., Awaitable[None]]:
+    async def _dynamic_f(self) -> None:
         self.info_stream(f"called {controller} f method: {method_name}")
         return await MethodType(method, controller)()
 
@@ -97,24 +114,25 @@ def _pick_command_f(method_name, method, controller):
     return _dynamic_f
 
 
-def _collect_dev_commands(mapping: Mapping) -> dict:
+def _collect_dev_commands(mapping: Mapping) -> dict[str, Any]:
     collection = {}
-    for s_map in mapping.get_controller_mappings():
-        path = s_map.controller.path
+    for single_mapping in mapping.get_controller_mappings():
+        path = single_mapping.controller.path
 
-        for name, method in s_map.command_methods.items():
+        for name, method in single_mapping.command_methods.items():
             instance = {}
             cmd_name = name.title().replace("_", "")
-            dev_cmd_name = path.upper() + "_" + cmd_name if path else cmd_name
-            f_args = (dev_cmd_name, method.fn, s_map.controller)
-            instance["f"] = _pick_command_f(*f_args)
+            d_cmd_name = path.upper() + "_" + cmd_name if path else cmd_name
+            instance["f"] = _pick_command_f(
+                d_cmd_name, method.fn, single_mapping.controller
+            )
             # instance["dtype_out"] = str  # Read return string for debug
-            collection[dev_cmd_name] = server.command(**instance)
+            collection[d_cmd_name] = server.command(**instance)
 
     return collection
 
 
-def _collect_dev_init(mapping: Mapping) -> dict:
+def _collect_dev_init(mapping: Mapping) -> dict[str, Callable]:
     async def init_device(self):
         await server.Device.init_device(self)
         self.set_state(DevState.ON)
@@ -123,18 +141,18 @@ def _collect_dev_init(mapping: Mapping) -> dict:
     return {"init_device": init_device}
 
 
-def _collect_dev_helpers(mapping: Mapping) -> dict:
+def _collect_dev_helpers(mapping: Mapping) -> dict[str, Any]:
     collection = {}
 
-    collection["green_mode"] = GreenMode.Asyncio
+    collection["green_mode"] = tango.GreenMode.Asyncio
 
     return collection
 
 
-def _collect_dsr_args(debug):
+def _collect_dsr_args(options: TangoDSROptions) -> List:
     args = []
 
-    if debug:
+    if options.debug:
         args.append("-v4")
 
     return args
@@ -144,7 +162,7 @@ class TangoDSR:
     def __init__(self, mapping: Mapping):
         self._mapping = mapping
 
-    def _link_process_tasks(self):
+    def _link_process_tasks(self) -> None:
         for single_mapping in self._mapping.get_controller_mappings():
             _link_single_controller_put_tasks(single_mapping)
             _link_attribute_sender_class(single_mapping)
@@ -157,7 +175,7 @@ class TangoDSR:
 
         dev_attributes = _collect_dev_attributes(self._mapping)
         dev_commands = _collect_dev_commands(self._mapping)
-        dev_properties = {}
+        dev_properties: dict = {}
         dev_init = _collect_dev_init(self._mapping)
         dev_helpers = _collect_dev_helpers(self._mapping)
 
@@ -169,18 +187,19 @@ class TangoDSR:
             **dev_helpers,
         }
 
-        pytango_class = type(options.dev_class, (server.Device,), class_body)
+        class_bases = (server.Device,)
+        pytango_class = type(options.dev_class, class_bases, class_body)
         register_dev(options.dev_name, options.dev_class, options.dsr_instance)
 
-        dsr_args = _collect_dsr_args(options.debug)
+        dsr_args = _collect_dsr_args(options)
 
         server.run(
             (pytango_class,),
             [options.dev_class, options.dsr_instance, *dsr_args],
-            )
+        )
 
 
-def register_dev(dev_name, dev_class, dsr_instance):
+def register_dev(dev_name: str, dev_class: str, dsr_instance: str) -> None:
     dsr_name = f"{dev_class}/{dsr_instance}"
     dev_info = DbDevInfo()
     dev_info.name = dev_name
